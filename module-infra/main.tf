ff1d9ea1627a2065db0f2fd1ce8357155e8e82ea
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-27 09:24:58 +0530

    user

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -14,14 +14,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+  description       = "ssh"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "app_port" {
   security_group_id = aws_security_group.tool.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = var.port
   ip_protocol       = "tcp"
   to_port           = var.port
+  description       = var.name
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_allow_all" {
+  security_group_id = aws_security_group.tool.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 
