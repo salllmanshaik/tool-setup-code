64f9ea499ca06f9b841de684c73c08ba04c84d01
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-27 08:17:10 +0530

    user

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.tool.id
-  cidr_ipv6         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = var.port
   ip_protocol       = "tcp"
   to_port           = var.port
