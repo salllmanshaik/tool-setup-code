b2774fb3bd90e72d10b0bb4f31c3ab1b3fc04cfd
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-27 14:28:59 +0530

    user

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -43,6 +43,13 @@ resource "aws_instance" "tool" {
   tags = {
     Name = var.name
   }
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
 }
 
 
