93f88a9e6fa52164c78d4acf5ffbdc2c4689e30e
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-28 07:50:45 +0530

    user

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -33,3 +33,21 @@ resource "vault_generic_secret" "ssh" {
 }
 EOT
 }
+
+resource "vault_mount" "roboshop-dev" {
+  path        = "roboshop-dev"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "Roboshop Dev secrets"
+}
+
+resource "vault_generic_secret" "roboshop-dev-cart" {
+  path = "${vault_mount.roboshop-dev.path}/cart"
+
+  data_json = <<EOT
+{
+  "username":   "ec2-user",
+  "password": "DevOps321"
+}
+EOT
+}
