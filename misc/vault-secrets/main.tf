d76993f532198fc384ff9b73a0adb115c0f04c82
salllmanshaik <hello.salmanshaik@gmail.com>
2025-04-01 13:58:01 +0530

    docker

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -79,22 +79,22 @@ resource "vault_generic_secret" "roboshop-dev-frontend" {
 "cart":   "http://cart-dev.salman06.shop:8080/",
 "shipping":   "http://shipping-dev.salman06.shop:8080/",
 "payment":   "http://payment-dev.salman06.shop:8080/",
-"CATALOGUE_HOST" = "catalogue-dev.salman06.shop",
-"CATALOGUE_PORT" = "8080",
-"USER_HOST" = "user-dev.salman06.shop",
-"USER_PORT" = "8080",
-"CART_HOST" = "cart-dev.salman06.shop",
-"CART_PORT" = "8080",
-"SHIPPING_HOST" = "shipping-dev.salman06.shop",
-"SHIPPING_PORT" = "8080",
-"PAYMENT_HOST" = "payment-dev.salman06.shop",
-"PAYMENT_PORT" = "8080"
+"CATALOGUE_HOST" : "catalogue-dev.salman06.shop",
+"CATALOGUE_PORT" : "8080",
+"USER_HOST" : "user-dev.salman06.shop",
+"USER_PORT" : "8080",
+"CART_HOST" : "cart-dev.salman06.shop",
+"CART_PORT" : "8080",
+"SHIPPING_HOST" : "shipping-dev.salman06.shop",
+"SHIPPING_PORT" : "8080",
+"PAYMENT_HOST" : "payment-dev.salman06.shop",
+"PAYMENT_PORT" : "8080"
 }
 EOT
 }
 
 resource "vault_generic_secret" "roboshop-dev-payment" {
-  path = "${vault_mount.roboshop-dev.path}/payment"
+  path : "${vault_mount.roboshop-dev.path}/payment"
 
   data_json = <<EOT
 {
